b276eebe3ad9c816a8e720e66cf3ce82240a2fbd
Rohit Kumar Gupta <rohitkumargupta1007@gmail.com>
2024-08-04 12:44:39 +0530

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,8 @@
 // its a console.log code
 console.log("Hello GitHub")
 console.log("Hello World") //2nd pull request
+
+
+console.log("Hello Github")
+console.log("Hello JS")
+console.log("Hello Java")
